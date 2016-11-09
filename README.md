8ded65cb7ad81864762ca40915014a0ad86a8994
Mattia Antonini <mattia.antonini1@studenti.unipr.it>
2016-11-09 16:09:35 +0100

    Created Installation script for IoTivity on RPi

--- README.md
+++ README.md
@@ -1,20 +1,30 @@
 AGILE - IoTivity Integration
 ======================
 
-This repository contains some IoTivity-based applications for different OSes.
+This repository contains instruction about the installation of [IoTivity] on a RaspberryPi and how to use it in conjunction with RIOT-OS.
 
-#RIOT-OS
+#IoTivity on RaspberryPi
+The installation of IoTivity on a RaspberryPi can be done by invoking
+```
+$ sudo -k ./install_iotivity.sh {Transport}
+```
+where Transport can be ALL/BT/BLE/IP/NFC/none. If you are undecided, choose ALL.
+It was tested on RaspberryPi 3 with Raspian Jessie version September 2016.
+
+#Applications
+##RIOT-OS
 The RIOT folder contains the IoTivity pkg for [RIOT-OS]. It allows to create embedded devices on which IoTivity runs. At the moment, the **Security** module is **disabled**! 
-##Integration in RIOT-OS
+###Integration in RIOT-OS
 In order to use IoTivity over RIOT-OS you have to copy the folder pkg/iotivity in the pkg folder inside of your RIOT root. Then you have to add the following line to the application's makefile
 ```
 USEPKG += iotivity
 ```
 The compilation system will do the rest.
-##Examples
+###Examples
 Inside the folder examples/iotivity-examples there are some examples to test the pkg.
 
-#Linux Example
+##Linux Example
 The Simple-Client-Linux folder contains an IoTivity-based client written in C++ for Linux. It can be used as test client for RIOT-based scenarios.
 
 [RIOT-OS]: https://github.com/RIOT-OS/RIOT
+[IoTivity]: https://www.iotivity.org
---
+++ install_iotivity.sh
@@ -0,0 +1,113 @@
+#!/bin/sh
+#####################################################################
+#                                                                   #
+# Installation Script for IoTivity                                  #
+#                                                                   #
+# Author: Mattia Antonini <mattia.antonini1@studenti.unipr.it>      #
+#                         <m.antonini@create-net.org>               #
+#                                                                   #
+# This script downloads, unzips, compiles IoTivity and then it      #
+# copies binaries in /usr. Tested with IoTivity 1.2.0.              #
+#####################################################################
+
+# Setup Params
+IOTIVITY_OS=linux          #linux/android/arduino/yocto/tizen
+IOTIVITY_ARCH=arm      #x86/x86_64/arm/arm-v7a/armeabi-v7a/arm64
+IOTIVITY_VERS=1.2.0
+IOTIVITY_TRANSPORT=ALL      #ALL/BT/BLE/IP/NFC/none
+IOTIVITY_SECURED=0         #0/1
+IOTIVITY_RELEASE=yes       #yes/no
+IOTIVITY_ROUTING=EP        #EP/GW
+IOTIVITY_BUILD_SAMPLE=OFF  #ON/OFF
+
+#Environment Vars
+IOTIVITY_WORKING_DIR=$(dirname $(readlink -f $0))"/iotivity_wdir"
+IOTIVITY_COMPILED_BINARIES_DIR=$(dirname $(readlink -f $0))"/iotivity_wdir/iotivity-"${IOTIVITY_VERS}"/out/"${IOTIVITY_OS}"/"${IOTIVITY_ARCH}"/release/"
+
+#Functions
+usage() {
+    echo "Usage: $0 <Transport>" 
+}
+
+checksudo() {
+    if [ $(id -u) -ne 0 ] ; then
+        echo "Please run as root."
+        echo "e.g. sudo -k "$0
+        exit 1
+    fi
+}
+
+review_params() {
+    echo "IoTivity will be installed with the following settings:"
+    echo ""
+    echo "TARGET_OS: "${IOTIVITY_OS}
+    echo "TARGET_ARCH: "${IOTIVITY_ARCH}
+    echo "TARGET_TRANSPORT: "${IOTIVITY_TRANSPORT}
+    echo "SECURED: "${IOTIVITY_SECURED}
+    echo "RELEASE: "${IOTIVITY_RELEASE}
+    echo "ROUTING: "${IOTIVITY_ROUTING}
+    echo "BUILD_SAMPLE":${IOTIVITY_BUILD_SAMPLE}
+    echo "IoTivity Version: "${IOTIVITY_VERS}
+    echo "IoTivity working directory: "${IOTIVITY_WORKING_DIR}
+    echo ""
+    read -p "If you agree type Y otherwise N: " opts
+
+    if [ "$opts" = "Y" ] || [ "$opts" = "y" ]; then
+        echo "Setup accepted..."
+    elif [ "$opts" = "N" ] || [ "$opts" = "n" ]; then
+        echo "Exiting..."
+        exit 1
+    else
+        echo "Choice unknown! Exiting..."
+        exit 1
+    fi
+}
+
+setup_dep() {
+    sudo apt-get update
+    sudo apt-get -y install scons build-essential libboost-dev libexpat1-dev libboost-thread-dev libboost-program-options-dev uuid-dev libssl-dev libglib2.0-dev
+}
+
+download_unzip() {
+    if [ ! -d "${IOTIVITY_WORKING_DIR}" ] ; then
+       mkdir ${IOTIVITY_WORKING_DIR}
+    fi
+
+    cd ${IOTIVITY_WORKING_DIR}
+
+    if [ ! -d "iotivity-${IOTIVITY_VERS}" ] ; then
+           wget -c http://mirrors.kernel.org/iotivity/${IOTIVITY_VERS}/iotivity-${IOTIVITY_VERS}.zip
+           unzip iotivity-${IOTIVITY_VERS}.zip
+    fi
+
+    cd iotivity-${IOTIVITY_VERS}
+    if [ ! -d "extlibs/tinycbor/tinycbor" ] ; then
+        git clone https://github.com/01org/tinycbor.git extlibs/tinycbor/tinycbor
+    fi
+    
+}
+
+compile() {
+    scons TARGET_ARCH=${IOTIVITY_ARCH} TARGET_OS=${IOTIVITY_OS} TARGET_TRANSPORT=${IOTIVITY_TRANSPORT} RELEASE=${IOTIVITY_RELEASE} SECURED=${IOTIVITY_SECURED} ROUTING=${IOTIVITY_ROUTING} BUILD_SAMPLE=${IOTIVITY_BUILD_SAMPLE}
+}
+
+copy_binaries(){
+    sudo cp ${IOTIVITY_COMPILED_BINARIES_DIR}/*.a /usr/lib
+    sudo cp ${IOTIVITY_COMPILED_BINARIES_DIR}/*.so /usr/lib
+
+    if [ ! -d "/usr/include/iotivity" ] ; then
+       sudo mkdir /usr/include/iotivity
+    fi
+    
+    sudo cp -r ${IOTIVITY_COMPILED_BINARIES_DIR}/include/resource /usr/include/iotivity/
+    sudo cp -r ${IOTIVITY_COMPILED_BINARIES_DIR}/include/service /usr/include/iotivity/
+    sudo cp -r ${IOTIVITY_COMPILED_BINARIES_DIR}/include/c_common /usr/include/iotivity/resource/
+}
+
+finished() {
+    echo "Installation Completed!"
+    echo "You can find an example here: https://github.com/Agile-IoT/agile-iotivity"
+    exit 0
+}
+
+checksudo && review_params && setup_dep && download_unzip && compile && copy_binaries && finished
